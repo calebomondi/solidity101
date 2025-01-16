5d8ee82d46ae60bf9e04a5d58ea51237c43c4d38
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-01-16 23:25:43 +0300

    added the sayHello function that concatinates a name to hello

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -1,7 +1,15 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.26;
 
 contract Hello {
-    string public welcom = "Hello Galaxy";
+    string public hello;
+
+    constructor(string memory name) {
+        hello = name;
+    }
+
+    function sayHello() public view returns (string memory) {
+         return string.concat("Hello there ", hello, ", Welocme to Human :)");
+    }
 }
